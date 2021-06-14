cfeacf893936072d9c37c8f9a68ddd41c3738c34
Maurizio <mauriziocasciano7@gmail.com>
2021-06-14 15:26:59 +0200

    Test refactoring

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 from stix2.v20.sdo import AttackPattern, CourseOfAction
-from stix2.utils import STIXdatetime
-
 from mitre.attack.ics.mitre_attack_ics import MitreAttackICS
-from domain.mitre.attack.attack_pattern_dto import AttackPatternDTO, SpecVersion
+from domain.mitre.attack.technique_dto import TechniqueDTO, SpecVersion
 
 if __name__ == '__main__':
     mitre_attack_ics = MitreAttackICS()
@@ -22,12 +20,12 @@ if __name__ == '__main__':
     # print(str(technique.get("modified").isoformat("T")) + "Z")
 
     # attack_pattern_dto: AttackPatternDTO = AttackPatternDTO().parse_obj(technique.__dict__)
-    attack_pattern_dto: AttackPatternDTO = AttackPatternDTO(spec_version=SpecVersion.field_2_0,
-                                                            created=str(technique.get("created").replace(tzinfo=None)
-                                                                        .isoformat("T")) + "Z",
-                                                            modified=str(technique.get("modified").replace(tzinfo=None)
-                                                                         .isoformat("T")) + "Z",
-                                                            **technique.__dict__)
+    attack_pattern_dto: TechniqueDTO = TechniqueDTO(spec_version=SpecVersion.field_2_0,
+                                                    created=str(technique.get("created").replace(tzinfo=None)
+                                                                .isoformat("T")) + "Z",
+                                                    modified=str(technique.get("modified").replace(tzinfo=None)
+                                                                 .isoformat("T")) + "Z",
+                                                    **technique.__dict__)
 
     print(attack_pattern_dto.json())
 
